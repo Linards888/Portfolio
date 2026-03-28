dd506a2ac0899d136577e8e121f14139888082f6
Linards888 <linardsbalodis2009@gmail.com>
2026-03-28 13:06:05 +0200

    page projects updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,50 +6,73 @@ const projects = [
     description:
       "Mana pirmā pieredze web izstrādē. Šajā projektā iemācījos pamatus par frontend izstrādi.",
     image: "/PortfolioIMG.png",
-    link: "https://github.com/Linards888/Portfolio",
+    buttons: [
+      {
+        text: "GitHub",
+        link: "https://github.com/Linards888/Portfolio",
+      },
+      {
+        text: "Live Demo",
+        link: "https://linardsb.xyz",
+      },
+    ],
   },
   {
     title: "Robotu Folkrace",
     description:
       "Robotu sacensību projekts Latvijā, kurā izstrādāju robota 3D konstrukciju, veidoju elektronikas shēmas un programmēju vadības sistēmu. Integrēju sensorus, izstrādāju kustības loģiku un optimizēju robota darbību sacensību apstākļiem.",
     image: "/folkrace.png",
-    link: "https://github.com/Linards888/Folkrace",
+    buttons: [
+      {
+        text: "GitHub",
+        link: "https://github.com/Linards888/Folkrace",
+      },
+      {
+        text: '"Pirmais Folkrace"',
+        link: "https://a360.co/3O1SIhj",
+      },
+      {
+        text: '"Brushless Folk"',
+        link: "https://a360.co/4bVA5Ug",
+      },
+      {
+        text: '"FolkNEW"',
+        link: "https://a360.co/47p1zAb",
+      },
+    ],
   },
   {
     title: "Antweight 454g",
     description:
       "Esmu izstrādājis dažādus antweight robotus kas ir piedalījušies LRČ (Latvijas robotikas čempionātos) un Robots kas parādīts bildē piedalijās RoboChallenge 2025 gadā un tika top 16 Eiropā",
     image: "/img1.jpg",
-    link: "https://github.com/",
+    buttons: [
+      {
+        text: '"Velvenis"',
+        link: "https://a360.co/4j2iNXR",
+      },
+    ],
   },
   {
     title: "DIY SIM racing",
     description:
       "Mēs izveidojām braukšanas simulatoru, veidojot 3D detaļas, integrējot elektroniku un konfigurējot vadības sistēmu (stūre, pedāļi u.c.). Piedalījos sistēmas uzbūvē un optimizācijā, lai nodrošinātu reālistisku un precīzu vadību. Pašlaik strādājam pie kameras integrācijas robotu folkreisa projektā un tās savienošanas ar SIM setup, lai varētu attālināti vadīt robotu un skatīt braucienu VR brillēs.",
     image: "/img2.jpg",
-    link: "https://github.com/",
+    buttons: [
+      {
+        text: "Github Repo",
+        link: "https://github.com/",
+      }
+    ],
   },
   {
-    title: "G_code expermenti",
+    title: "G-code eksperimenti",
     description:
       "Šajā momentā, kā paildus projektu, es apgūstu prfesionālu frēzēšanu. Es cenšos izprast kā uztaisa profesionālu CNC programmu priekš dažādiem mērķiem un vajadzībām.",
-    image: "/folkrace.png",
-    link: "https://github.com/",
-  },
-  {
-    title: "Robot sensors test bench",
-    description:
-      "Testēju dažādus sensorus (IR, ultraskaņa) un kalibrēju tos robotiem.",
-    image: "/folkrace.png",
-    link: "https://github.com/",
-  },
-  {
-    title: "AI kodēšana un mācīšana",
-    description:
-      "Projekts, kur mācos un izstrādāju AI botu – programmēju, trenēju un testēju. Progress lēns, bet notiek 😅",
-    image: "/folkrace.png",
-    link: "https://github.com/Linards888/BotMiku",
-  },
+    image: "/freze.jpg",
+    buttons: [
+    ],
+  }
 ];
 
 export default function Projects() {
@@ -65,7 +88,7 @@ export default function Projects() {
             className={`project-row ${isReverse ? "reverse" : ""}`}
             key={index}
           >
-            {}
+            {/* IMAGE */}
             {project.image && (
               <div className="image-box">
                 <Image
@@ -77,19 +100,24 @@ export default function Projects() {
               </div>
             )}
 
-            {}
+            {/* TEXT */}
             <div className="card">
               <h2>{project.title}</h2>
               <p>{project.description}</p>
 
-              <a
-                href={project.link}
-                className="button"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub Repo
-              </a>
+              <div className="button-group">
+                {project.buttons.map((btn, i) => (
+                  <a
+                    key={i}
+                    href={btn.link}
+                    className="button"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {btn.text}
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
         );
